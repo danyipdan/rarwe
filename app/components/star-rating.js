912c4efc67ec5dda7f4e88707a0d1f4aff568d96
Dan Yip <Dan.Yip@3plearning.com>
2022-06-17 14:52:00 +1000

    feat: janky star rating

--- app/components/star-rating.js
+++ app/components/star-rating.js
@@ -1,3 +1,23 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class StarRatingComponent extends Component {}
+export default class StarRatingComponent extends Component {
+  @tracked rating = this.args.rating;
+  @tracked maxRating = 5;
+
+  get stars() {
+    const starsArray = [];
+
+    for (let i = 1; i <= this.maxRating; i++) {
+      starsArray.push({ rating: i, isFull: this.rating >= i });
+    }
+    return starsArray;
+  }
+
+  @action setRating(newRating) {
+    console.log({ newRating });
+    this.args.item.set('rating', newRating);
+    this.rating = newRating;
+  }
+}
