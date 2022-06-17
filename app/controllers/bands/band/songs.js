acf1d7556251f1ca755257d4cef5d7e90a58e1ed
Dan Yip <Dan.Yip@3plearning.com>
2022-06-17 11:20:02 +1000

    feat: add song functionality

---
+++ app/controllers/bands/band/songs.js
@@ -0,0 +1,32 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { empty } from '@ember/object/computed';
+import Song from '../../../models/song';
+
+export default class SongsController extends Controller {
+  isAddingSong = false;
+  newSongTitle = '';
+
+  @empty('newSongTitle') isAddButtonDisabled
+
+  @action updateSongTitle(e){
+    console.log(this.newSongTitle);
+    this.set('newSongTitle', e.target.value)
+  }
+  @action addSong() {
+    this.set('isAddingSong', true);
+  }
+
+  @action saveSong(e) {
+    e.preventDefault()
+    console.log('saveSong()', this.newSongTitle);
+    let newSong = Song.create({ title: this.newSongTitle });
+    this.model.songs.pushObject(newSong);
+    this.set('newSongTitle', '');
+  }
+
+  @action cancelAddSong() {
+    this.set('isAddingSong', false);
+  }
+
+}
