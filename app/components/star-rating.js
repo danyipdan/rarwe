01ffa96a27b51d31424d85ed1d25f54dd7e17f02
Dan Yip <Dan.Yip@3plearning.com>
2022-06-19 15:18:50 +1000

    style: tidy up star rating

--- app/components/star-rating.js
+++ app/components/star-rating.js
@@ -2,19 +2,22 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class StarRatingComponent extends Component {
-
+  
   get stars() {
-    const {rating} = this.args
+    const { rating } = this.args;
     const maxRating = 5;
     const starsArray = [];
 
     for (let i = 1; i <= maxRating; i++) {
       starsArray.push({ rating: i, isFull: rating >= i });
     }
+
     return starsArray;
   }
 
   @action setRating(newRating) {
-    this.args.updateStar(this.args.item, newRating );
+    const { item, updateStar } = this.args;
+
+    updateStar(item, newRating);
   }
 }
